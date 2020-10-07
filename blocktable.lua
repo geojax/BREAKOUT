8df53c089b0dc95dbab21ef42dcf744e123990a8
geojax <50026101+geojax@users.noreply.github.com>
2020-10-06 17:38:23 -0700

    Indexing from 0 instead of 1
    Indexing from 1 was creating so problems, so collisions should be easier with indexing from 0

--- blocktable.lua
+++ blocktable.lua
@@ -4,27 +4,27 @@ blocktable.width = 8
 block_height = 10
 block_width = 50
 
-for y = 1, blocktable.height do
-  for x = 1, blocktable.width do
+for y = 0, blocktable.height - 1 do
+  for x = 0, blocktable.width - 1 do
     blocktable[y * blocktable.width + x] = y
   end
 end
 
 function blocktable.draw()
-  for y = 1, blocktable.height do
-    for x = 1, blocktable.width do
+  for y = 0, blocktable.height - 1 do
+    for x = 0, blocktable.width - 1 do
       local currentBlock = blocktable[y * blocktable.width + x]
-      if currentBlock == 4 then
+      if currentBlock == 3 then
         love.graphics.setColor(1, 0, 0)
-      elseif currentBlock == 3 then
-        love.graphics.setColor(0, 1, 0)
       elseif currentBlock == 2 then
-        love.graphics.setColor(0, 0, 1)
+        love.graphics.setColor(0, 1, 0)
       elseif currentBlock == 1 then
+        love.graphics.setColor(0, 0, 1)
+      elseif currentBlock == 0 then
         love.graphics.setColor(1, 0, 1)
       end
-      if currentBlock > 0 then
-        love.graphics.rectangle("fill", x * block_width - block_width, y * block_height, block_width - 1, block_height)
+      if currentBlock >= 0 then
+        love.graphics.rectangle("fill", x * block_width, y * block_height, block_width - 1, block_height)
       end
     end
   end
